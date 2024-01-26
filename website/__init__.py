c4a0d29539fb1a2984f5dbdd0885a4b26e265f1c
Sam Nguyen <75660550+Kai0802@users.noreply.github.com>
2024-01-26 13:24:39 -0500

    add database and flashing messages for forms

--- website/__init__.py
+++ website/__init__.py
@@ -2,12 +2,22 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+# this is the new database, we initilize it like the line below
+db = SQLAlchemy()
+# this is our database's name
+DB_NAME = "database.db"
 
 
 def create_app():
     # __name__ represents the name of the file that you run
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hidahslkdwakld dhwkajdhsad'
+    # we a file to store all the information, therefore, we need to register it
+    # this is saying that my database is stored in the website folder. We're just telling flask where this is located.
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)  # this is just connecting the app with the database
     # register the blueprint. the url_prefix mean that if you want to access all the
     app.register_blueprint(views, url_prefix='/')
     # urls inside of this blueprint, you must start with the / first. For example: inside of views blueprint we have a route like this
--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+# when going to the url it will be the get request, and when click on submit it will be the post request
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     # the text and user are two variables that being passed from backend
     # to front end
+    data = request.form
+    print(data)
     return render_template("login.html.j2", boolean=True)
 
 
@@ -15,6 +18,24 @@ def logout():
     return "<p>Log out<p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters.", category='error')
+        elif len(firstName) < 2:
+            flash('first name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters long.', category='error')
+        else:
+            flash("Account created!", category='success')
+
+        print(request.form)
     return render_template("sign_up.html.j2")
